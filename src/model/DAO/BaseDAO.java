2ac7db474c6e0d030d33a4b5121ccb42ff3b43ee
RiadOliveira <riad.oliveira@hotmail.com>
2022-03-04 00:33:21 -0300

    enviroment variables to database password

--- src/model/DAO/BaseDAO.java
+++ src/model/DAO/BaseDAO.java
@@ -1,5 +1,6 @@
 package model.DAO;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,7 +9,7 @@ public abstract class BaseDAO<VO> implements IBaseDAO<VO> {
     private static Connection connection = null;
     private static final String url = "jdbc:postgresql://localhost:5432/Room4ME";
     private static final String user = "postgres";
-    private static final String password = "";
+    private static final String password = Dotenv.load().get("DATABASE_PASSWORD");
 	
 	public static Connection getConnection() throws SQLException {
 		if (connection == null) {
