73463241f229ee5119d1470d5277a4845254a5b6
DaviEmanuelll <daviemanuelll04@gmail.com>
2022-05-13 13:07:53 -0300

    Login Controller

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,17 +1,26 @@
 package controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class LoginController extends changeScreen{
+    
+    @FXML
+    private Button loginButton;
 
     @FXML
-    private Button buttonLogin;
+    private Button signUpButton;
 
     @FXML
-    void singUp(ActionEvent event) {
-        
+    void login(ActionEvent event) {
+
     }
 
+    @FXML
+    void signUp(ActionEvent event) {
+
+    }
 }
+
 
